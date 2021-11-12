573f6f4e613b8fcf73fb011ff24afaf4f7a9d8cd
JenniferNguy <jennifernguyen934@gmail.com>
2021-11-12 11:09:14 -0500

    Fix Header

--- src/Scripts/app.js
+++ src/Scripts/app.js
@@ -1,8 +1,9 @@
-(()=>{
-    function Start()
-    {
-        console.log("App Started...")
+// IIFE- IMMIDEATELY INVOKED FUNCTION EXPRESSION
+(() => {
+
+    function Start(){
+        console.log('App Started...')
     }
 
-window.addEventListener('load', Start)
+    window.addEventListener('load', Start);
 })();
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path:'create-survey', component:CreateSurveyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
